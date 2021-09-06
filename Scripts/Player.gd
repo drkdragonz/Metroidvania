3b4098b7334087275de0cfa7e948861504518f18
drkdragonz <foxgamer098@gmail.com>
2021-09-06 20:45:22 +0100

    cleanup

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,14 +4,17 @@ export var jump_force = 300
 export var _speed = 200
 export var _gravity = 800
  
-export var can_jump_start = 0
+export var can_jump_start = 3
  
-var can_jump = 1
+var can_jump = 2
  
 var _velocity = Vector2()
 var _floor = Vector2(0, -1)
 onready var animated_sprite = $AnimatedSprite
  
+var dashDirection = Vector2(1, 0)
+var canDash = false
+var dashing = false
  
 enum {
 	IDLE
@@ -20,19 +23,22 @@ enum {
 }
  
 var _state: int = IDLE
+var previous_state = null
  
 func _ready():
 	can_jump = can_jump_start
 	print(can_jump)
  
 func _physics_process(delta):
+	print(_state)
 	_velocity.x = move_direction().x * _speed
 	_velocity.y += _gravity
 	flip_sprite()
 	
-	if can_jump and Input.is_action_just_pressed("up"):
+	
+	if Input.is_action_just_pressed("up") and can_jump > 0:
 		_velocity.y = -jump_force
-		can_jump = false
+		can_jump -= 1
 		change_state(AIR)
 	
 	match _state:
@@ -40,6 +46,8 @@ func _physics_process(delta):
 			animated_sprite.play("Idle")
 			if is_on_floor() and _velocity.x:
 				change_state(RUN)
+			if is_on_floor():
+				can_jump = 3
 		RUN:
 			animated_sprite.play("Run")
 			if is_on_floor() and not _velocity.x:
@@ -49,7 +57,6 @@ func _physics_process(delta):
 			if is_on_floor() and _velocity.y > 0:
 				change_state(IDLE)
 				can_jump = can_jump_start
-	
 	_velocity = move_and_slide(_velocity, _floor)
  
  
@@ -58,8 +65,9 @@ func move_direction() -> Vector2:
 		Input.get_action_strength("right") - Input.get_action_strength("left"), 0)
  
 func change_state(target_state: int):
+	_state == previous_state
 	_state = target_state
- 
+
 func flip_sprite() -> void:
 	var animated_sprite = $AnimatedSprite
 	if move_direction().x < 0:
