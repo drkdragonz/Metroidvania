693cac862538ddfb5626329b439039c43e4dc25c
drkdragonz <foxgamer098@gmail.com>
2021-10-02 20:07:19 +0100

    NPC Completed with dialogue and talking cooldowns

--- NPC.gd
+++ NPC.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+var Dialog = preload("res://DialogBox.tscn")
 var active = false
 
 func _ready():
@@ -10,12 +10,24 @@ func _ready():
 func _process(delta):
 	$QuestionMark.visible = active
 
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept") and active == true:
+		var DIALOG = Dialog.instance()
+		get_parent().add_child(DIALOG)
+		$CollisionShape2D.disabled = true
+		$Timer.start()
 
 func _on_NPC_body_entered(body):
 	if body.name == "Player":
 		active = true
+		
+	
 
 
 func _on_NPC_body_exited(body):
 	if body.name == "Player":
 		active = false
+
+
+func _on_Timer_timeout():
+	$CollisionShape2D.disabled = false
