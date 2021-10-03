0233df963329c1452aae4b527499b5e24959d870
drkdragonz <foxgamer098@gmail.com>
2021-10-03 14:32:17 +0100

    gonna put this project on hold. this is the latest state

--- Scenes/SceneSwitch.gd
+++ Scenes/SceneSwitch.gd
@@ -1,11 +1,4 @@
-extends Control
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,5 +9,9 @@ func _ready():
 #	pass
 
 
-func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/StarterArea.tscn")
+func _on_Area2D_body_entered(body):
+	$AnimationPlayer.play("Transition")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://Scenes/World.tscn")
